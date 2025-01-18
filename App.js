8ac91a4a6cc4e7c56f86d87608ae40736212bc71
Navneet Gupta <navneetguptaid7@gmail.com>
2025-01-19 04:11:26 +0530

    Add npm configuration, initialize our React project.

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-const element = (
-    <div>
-        <h1 className="heading">Namaste React 🙏</h1>
-        <p>This is a paragraph inside a complex element.</p>
-        <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-        </ul>
-    </div>
+import React from "react";
+import ReactDOM from 'react-dom';
+
+const element = React.createElement(
+    'div',
+    null,
+    React.createElement('h1', { className: 'heading' }, 'Namaste React 🙏'),
+    React.createElement('p', null, 'This is a paragraph inside a complex element.'),
+    React.createElement(
+        'ul',
+        null,
+        React.createElement('li', null, 'Item 1'),
+        React.createElement('li', null, 'Item 2'),
+        React.createElement('li', null, 'Item 3')
+    )
 );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
