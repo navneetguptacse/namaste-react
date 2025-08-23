ba47c298455ae7c1bd2beaf59cb6ad61c786aabd
Navneet Gupta <navneetguptacse@gmail.com>
2025-08-23 10:09:39 +0530

    Hooks: useState(), useEffect() essentials

--- namaste-app/swiggy-app/src/App.jsx
+++ namaste-app/swiggy-app/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Body from "./components/Body";
+import Hooks from "./components/Hooks";
+// import Body from "./components/Body";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
+      {/* <Body /> */}
+      <Hooks />
       <Footer />
     </div>
   );
