b7b39ae9ccc6670c54466568563fc7f61e184530
Navneet Gupta <navneetguptacse@gmail.com>
2025-08-31 23:18:12 +0530

    Add state variable in class-based-component.

--- namaste-app/swiggy-app/src/classes/UserClass.jsx
+++ namaste-app/swiggy-app/src/classes/UserClass.jsx
@@ -3,16 +3,43 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
-    console.log(props);
+    this.state = { count: 0, count2: 0 };
   }
   render() {
     const { name, location } = this.props;
+    const { count, count2 } = this.state;
     return (
       <div className="card card-content">
         <h4>Name: {name}</h4>
         <h4>Location: {location}</h4>
         <h4>Contact: @navneetgupta</h4>
         <p>About: I'm a Software Engineer.</p>
+        <hr />
+        <h4>
+          Count: {count} and Count2: {count2}
+        </h4>
+        <button
+          onClick={() => {
+            this.setState({ count: count + 1 });
+          }}
+        >
+          Click (class)
+        </button>
+        <button
+          onClick={() => {
+            this.setState({ count2: count2 + 2 });
+          }}
+        >
+          Click2 (class)
+        </button>
+        <hr />
+        <button
+          onClick={() => {
+            this.setState({ count: 0, count2: 0 });
+          }}
+        >
+          Reset
+        </button>
       </div>
     );
   }
--- namaste-app/swiggy-app/src/functions/User.jsx
+++ namaste-app/swiggy-app/src/functions/User.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
+
 const User = ({ name, location }) => {
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+
   return (
     <div className="card card-content">
       <h4>Name: {name}</h4>
       <h4>Location: {location}</h4>
       <h4>Contact: @navneetgupta</h4>
       <p>About: I'm a Software Engineer.</p>
+      <hr />
+      <h4>
+        Count: {count} and Count2: {count2}
+      </h4>
+      <button onClick={() => setCount(count + 1)}>Click (function)</button>
+      <button onClick={() => setCount2(count2 + 2)}>Click2 (function)</button>
+      <hr />
+      <button
+        onClick={() => {
+          setCount(0);
+          setCount2(0);
+        }}
+      >
+        Reset
+      </button>
     </div>
   );
 };
